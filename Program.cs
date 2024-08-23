90c13cdd5ed5c99556c90f9a95478bbc62bfc673
tawainfer <tawainfer@gmail.com>
2024-08-23 21:35:52 +0900

    202408231

--- Program.cs
+++ Program.cs
@@ -31,6 +31,21 @@ public class Program
 {
     private Stopwatch _stopwatch;
     private TimeSpan _timeout;
+    private int _n;
+    private int _m;
+    private int _t;
+    private int _la;
+    private int _lb;
+    private List<int> _u;
+    private List<int> _v;
+    private List<List<int>> _g;
+    private List<int> _order;
+    private List<int> _x;
+    private List<int> _y;
+    private List<int> _a;
+    private List<int> _b;
+    private int _cp = 0;
+    private int _epIdx = 0;
 
     public Stopwatch Stopwatch { get { return _stopwatch; } }
     public TimeSpan Timeout { get { return _timeout; } }
@@ -43,12 +58,88 @@ public class Program
     public Program()
     {
         _stopwatch = Stopwatch.StartNew();
-        _timeout = TimeSpan.FromMilliseconds(1900);
+        _timeout = TimeSpan.FromMilliseconds(2900);
+        _u = new();
+        _v = new();
+        _order = new();
+        _x = new();
+        _y = new();
+        _a = new();
+        _b = new();
+        _g = new();
         Solve();
     }
 
     public void Solve()
     {
-        while (Stopwatch.Elapsed <= Timeout) { }
+        var tmp = ReadLine()!.Split().Select(int.Parse).ToArray();
+        (_n, _m, _t, _la, _lb) = (tmp[0], tmp[1], tmp[2], tmp[3], tmp[4]);
+
+        _g = new();
+        for (int _ = 0; _ < _n; _++) _g.Add(new());
+
+        for (int _ = 0; _ < _m; _++)
+        {
+            tmp = ReadLine()!.Split().Select(int.Parse).ToArray();
+            _u.Add(tmp[0]);
+            _v.Add(tmp[1]);
+            _g[tmp[0]].Add(tmp[1]);
+            _g[tmp[1]].Add(tmp[0]);
+        }
+        for (int i = 0; i < _n; i++) _g[i].Sort();
+
+        _order = ReadLine()!.Split().Select(int.Parse).ToList();
+
+        for (int _ = 0; _ < _n; _++)
+        {
+            tmp = ReadLine()!.Split().Select(int.Parse).ToArray();
+            _x.Add(tmp[0]);
+            _y.Add(tmp[1]);
+        }
+
+        _a = new();
+        for (int i = 0; i < _la; i++) _a.Add(i % _n);
+
+        _b = new();
+        for (int i = 0; i < _lb; i++) _b.Add(-1);
+
+        WriteLine(string.Join(' ', _a));
+
+        foreach (int ep in _order)
+        {
+            List<bool> seen = new();
+            for (int _ = 0; _ < _n; _++) seen.Add(false);
+            seen[_cp] = true;
+            Queue<(int, List<int>)> q = new();
+            q.Enqueue((_cp, new() { _cp, }));
+
+            List<int> confirmedRoot = new();
+            while (q.Count >= 1)
+            {
+                (int u, List<int> root) = q.Dequeue();
+                if (u == ep)
+                {
+                    confirmedRoot = root;
+                    break;
+                }
+
+                foreach (int v in _g[u])
+                {
+                    if (seen[v]) continue;
+                    seen[v] = true;
+                    List<int> newRoot = new();
+                    newRoot.AddRange(root);
+                    newRoot.Add(v);
+                    q.Enqueue((v, newRoot));
+                }
+            }
+
+            for (int i = 1; i < confirmedRoot.Count; i++)
+            {
+                WriteLine($"s {1} {_a.IndexOf(confirmedRoot[i])} {0}");
+                WriteLine($"m {confirmedRoot[i]}");
+                _cp = confirmedRoot[i];
+            }
+        }
     }
 }
