476679d436debb57b1d0bfab8142470b46c4dfcf
tawainfer <tawainfer@gmail.com>
2024-11-10 15:24:41 +0900

    202411101

--- Program.cs
+++ Program.cs
@@ -4,18 +4,18 @@ using static System.Console;
 
 public static class Extensions
 {
-  private static Random r = new Random();
+    private static Random r = new Random();
 
-  public static void Shuffle<T>(this IList<T> v)
-  {
-    for (int i = v.Count - 1; i > 0; i--)
+    public static void Shuffle<T>(this IList<T> v)
     {
-      int j = r.Next(0, i + 1);
-      var tmp = v[i];
-      v[i] = v[j];
-      v[j] = tmp;
+        for (int i = v.Count - 1; i > 0; i--)
+        {
+            int j = r.Next(0, i + 1);
+            var tmp = v[i];
+            v[i] = v[j];
+            v[j] = tmp;
+        }
     }
-  }
 }
 
 // public static class DeepCopy
@@ -29,37 +29,53 @@ public static class Extensions
 
 public static class SharedStopwatch
 {
-  private static Stopwatch _stopwatch = new Stopwatch();
-  private static double _frequency = Stopwatch.Frequency;
+    private static Stopwatch _stopwatch = new Stopwatch();
+    private static double _frequency = Stopwatch.Frequency;
 
-  public static void Start() => _stopwatch.Start();
-  public static void Stop() => _stopwatch.Stop();
-  public static void Reset() => _stopwatch.Reset();
-  public static long ElapsedMilliseconds() => _stopwatch.ElapsedMilliseconds;
-  public static long ElapsedMicroseconds() => (long)(_stopwatch.ElapsedTicks / _frequency * 1_000_000);
+    public static void Start() => _stopwatch.Start();
+    public static void Stop() => _stopwatch.Stop();
+    public static void Reset() => _stopwatch.Reset();
+    public static long ElapsedMilliseconds() => _stopwatch.ElapsedMilliseconds;
+    public static long ElapsedMicroseconds() => (long)(_stopwatch.ElapsedTicks / _frequency * 1_000_000);
 }
 
 public class Program
 {
-  public static void Main(string[] args)
-  {
-    new Program();
-  }
+    private int _n;
+    private List<int> _x = new();
+    private List<int> _y = new();
 
-  public Program()
-  {
-    SharedStopwatch.Start();
-    Input();
-    Solve();
-  }
+    public static void Main(string[] args)
+    {
+        new Program();
+    }
 
-  public void Input()
-  {
-  }
+    public Program()
+    {
+        SharedStopwatch.Start();
+        Input();
+        Solve();
+    }
 
-  public void Solve()
-  {
-    while (SharedStopwatch.ElapsedMilliseconds() <= 1900) ;
-    WriteLine($"{SharedStopwatch.ElapsedMilliseconds()}ms");
-  }
+    public void Input()
+    {
+        _n = int.Parse(ReadLine()!);
+        _x = new();
+        _y = new();
+        for (int _ = 0; _ < _n; _++)
+        {
+            int[] buf = ReadLine()!.Split().Select(int.Parse).ToArray();
+            _x.Add(buf[0]);
+            _y.Add(buf[1]);
+        }
+    }
+
+    public void Solve()
+    {
+        WriteLine(4);
+        WriteLine($"{0} {0}");
+        WriteLine($"{0} {100000}");
+        WriteLine($"{100000} {100000}");
+        WriteLine($"{100000} {0}");
+    }
 }
---
+++ input.py
@@ -0,0 +1,36 @@
+import sys
+from pathlib import Path
+from collections import deque
+
+FILE = None
+FILE_READLINES = None
+
+def read_line():
+  global FILE
+  global FILE_READLINES
+  
+  if FILE:
+    if FILE_READLINES is None:
+      with open(Path(__file__).parent / 'in' / FILE, 'r') as rf:
+        FILE_READLINES = deque(rf.readlines())
+    return FILE_READLINES.popleft().strip()
+
+  return input()
+
+if __name__ == '__main__':
+  if len(sys.argv) >= 2:
+    FILE = sys.argv[1].strip()
+
+  n = int(read_line())
+  print(n)
+
+  x = list()
+  y = list()
+  for _ in range(n):
+    a, b = map(int, read_line().split())
+    x.append(a)
+    y.append(b)
+    print(a, b)
+
+  for _ in range(2 * n):
+    print(read_line())
